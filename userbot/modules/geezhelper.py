10a2c1f00582f69ca63d6a9ae458fb639a8f96a0
VickyMIPA <83190867+VickyMIPA@users.noreply.github.com>
2021-07-01 23:06:02 +0700

    Update geezhelper.py

--- userbot/modules/geezhelper.py
+++ userbot/modules/geezhelper.py
@@ -12,9 +12,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/VckyouuBitch)"
-        "\n[Repo](https://github.com/Vckyou/Geez-UserBot)"
-        "\n[Instagram](instagram.com/Vckyouuu)")
+        "\n[Telegram](t.me/xenxust)"
+        "\n[Repo](https://github.com/VickyMIPA/Nomercy-UserBot)"
+        "\n[Instagram](instagram.com/pikipoi_)")
 
 
 @register(outgoing=True, pattern="^.vars$")
@@ -27,7 +27,7 @@ async def var(m):
 CMD_HELP.update({
     "helper":
     "`.lhelp`\
-\nUsage: Bantuan Untuk Geez-Project.\
+\nUsage: Bantuan Untuk Nomercy-Project.\
 \n`.vars`\
 \nUsage: Melihat Daftar Vars."
 })
